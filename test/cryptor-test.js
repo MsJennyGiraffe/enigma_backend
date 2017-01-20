edfa19750187c8a9d3d881954ebafd545e72d8b6
Jennifer <jennifersoden@hotmail.com>
2017-01-20 15:45:57 -0700

    Add tests for cryptor and range

--- test/cryptor-test.js
+++ test/cryptor-test.js
@@ -1,16 +1,53 @@
 const assert = require('assert');
 const Cryptor = require('../lib/cryptor')
 
-describe('Encrypt', function() {
-  describe('#range()', function() {
-    it('should return an array of a character within a range', function() {
-      var cryptor = new Cryptor();
-      assert.deepEqual(["A"], cryptor.range("A", "A"));
+describe("Cryptor", function() {
+
+  describe("#encryptedIndex()", function() {
+    it("should return the encrypted character's new index", function() {
+      var cryptor = new Cryptor({rotations: [1, 2, 3, 4], cryptType: "encrypt"});
+      assert.equal(2, cryptor.encryptedIndex(1, 1));
     });
 
-    it('should return an array of characters within a range', function() {
-      var cryptor = new Cryptor();
-      assert.deepEqual([ 'a', 'b', 'c', 'd', 'e' ], cryptor.range("a", "e"))
+    it("should return the encrypted character's new index for larger numbers", function() {
+      var cryptor = new Cryptor({rotations: [1, 2, 3, 4], cryptType: "encrypt"});
+      assert.equal(23, cryptor.encryptedIndex(1, 100));
+    });
+  });
+
+  describe("#decryptedIndex()", function() {
+    it("should return the decrypted character's new index", function() {
+      var cryptor = new Cryptor({rotations: [1, 2, 3, 4], cryptType: "encrypt"});
+      assert.equal(1, cryptor.decryptedIndex(2, 1));
+    });
+
+    it("should return the decrypted character's new index for larger numbers", function() {
+      var cryptor = new Cryptor({rotations: [1, 2, 3, 4], cryptType: "encrypt"});
+      assert.equal(1, cryptor.decryptedIndex(23, 100));
+    });
+  });
+
+  describe("#rotateCharacter()", function() {
+    it("should return the encrypted character", function() {
+      var cryptor = new Cryptor({rotations: [1, 2, 3, 4], cryptType: "encrypt"});
+      assert.equal("b", cryptor.rotateCharacter("a", 1));
+    });
+
+    it("should return the decrypted character", function() {
+      var cryptor = new Cryptor({rotations: [1, 2, 3, 4], cryptType: "decrypt"});
+      assert.equal("z", cryptor.rotateCharacter("a", 1));
+    });
+  });
+
+  describe("#rotateMessage()", function() {
+    it("should return the encrypted message", function() {
+      var cryptor = new Cryptor({rotations: [1, 2, 3, 4], cryptType: "encrypt"});
+      assert.equal("igopp", cryptor.rotateMessage("hello"));
+    });
+
+    it("should return the decrypted message", function() {
+      var cryptor = new Cryptor({rotations: [1, 2, 3, 4], cryptType: "decrypt"});
+      assert.equal("hello", cryptor.rotateMessage("igopp"));
     });
   });
 });
