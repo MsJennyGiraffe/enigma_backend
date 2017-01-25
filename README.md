f748d7e28bb643627dfe45072c3baf797b7c9d95
thomp <thompickett@gmail.com>
2017-01-25 12:06:19 -0700

    Add show endpoints to API

--- README.md
+++ README.md
@@ -5,23 +5,49 @@ Project based off of a Turing School [project](https://github.com/turingschool/c
 ## Setup
 Go to project and clone project
 ```
-
+$ git clone git@github.com:MsJennyGiraffe/enigma_backend.git
 ```
 
-# enigma_backend
-Install Mongo.
+Install Mongo with homebrew.
+```
+$ brew install mongodb
+```
 
 Start mongodb
 
 From project root folder:
 ```
-mkdir database
-```
-```
-mongod --dbpath=./database
+$ mkdir database
 ```
 
-Start locally
 ```
-MONGODB_URI="mongodb://localhost:27017/database" npm start
+$ mongod --dbpath=./database
+```
+
+Start locally in another terminal window
+```
+$ MONGODB_URI="mongodb://localhost:27017/database" npm start
+```
+
+To test creating an encrypts entry in the db locally
+From another terminal window
+
+```
+$ curl -H "Content-Type: application/json" -d '{"originalMessage": "trees"}' http://localhost:8080/encrypts
+```
+
+Then from a browser visit `http://localhost:8080/encrypts`
+
+You should see
+
+```
+[
+- {
+  _id: "5882b06ad6049c625ea98c36",
+  originalMessage: "trees",
+  createDate: "2017-01-21T00:50:50.582Z",
+  messageType: "encrypted",
+  encryptedMessage: "xzodw"
+  }
+]
 ```
--- server.js
+++ server.js
@@ -43,11 +43,6 @@ function handleError(res, reason, message, code) {
   res.status(code || 500).json({"error": message});
 }
 
-/*  "/contacts"
- *    GET: finds all contacts
- *    POST: creates a new contact
- */
-
 app.get("/encrypts", function(req, res) {
   db.collection(ENCRYPTED_COLLECTION).find({}).toArray(function(err, docs) {
     if (err) {
@@ -78,44 +73,15 @@ app.post("/encrypts", function(req, res) {
   }
 });
 
-/*  "/contacts/:id"
- *    GET: find contact by id
- *    PUT: update contact by id
- *    DELETE: deletes contact by id
- */
-
-// app.get("/contacts/:id", function(req, res) {
-//   db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
-//     if (err) {
-//       handleError(res, err.message, "Failed to get contact");
-//     } else {
-//       res.status(200).json(doc);
-//     }
-//   });
-// });
-//
-// app.put("/contacts/:id", function(req, res) {
-//   let updateDoc = req.body;
-//   delete updateDoc._id;
-//
-//   db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
-//     if (err) {
-//       handleError(res, err.message, "Failed to update contact");
-//     } else {
-//       res.status(204).end();
-//     }
-//   });
-// });
-//
-// app.delete("/contacts/:id", function(req, res) {
-//   db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
-//     if (err) {
-//       handleError(res, err.message, "Failed to delete contact");
-//     } else {
-//       res.status(204).end();
-//     }
-//   });
-// });
+app.get("/encrypts/:id", function(req, res) {
+  db.collection(ENCRYPTED_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get message");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
+});
 
 app.get("/decrypts", function(req, res) {
   db.collection(DECRYPTED_COLLECTION).find({}).toArray(function(err, docs) {
@@ -145,4 +111,14 @@ app.post("/decrypts", function(req, res) {
       }
     });
   }
+});
+
+app.get("/decrypts/:id", function(req, res) {
+  db.collection(DECRYPTED_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get message");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
 });
